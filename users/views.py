978cb9fbffa880f74c4360735f0054c1862b7079
NattanSilva <natanxboy@gmail.com>
2023-02-14 14:21:21 -0300

    Add: task 1 complete

---
+++ users/views.py
@@ -0,0 +1,13 @@
+from rest_framework.views import APIView, Request, Response, status
+from .serializers import UserSerializer
+
+
+class UserView(APIView):
+    def post(self, request: Request) -> Response:
+        serializer = UserSerializer(data=request.data)
+
+        serializer.is_valid(raise_exception=True)
+
+        serializer.save()
+
+        return Response(serializer.data, status.HTTP_201_CREATED)
