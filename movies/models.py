d59db2977babf305f51ac70f6917663e86b8bdd0
NattanSilva <natanxboy@gmail.com>
2023-02-15 17:03:19 -0300

    Add: task 3 complete

--- movies/models.py
+++ movies/models.py
@@ -26,3 +26,29 @@ class Movie(models.Model):
         related_name="movies",
         null=False
     )
+
+    orders = models.ManyToManyField(
+        "users.User",
+        through="movies.MovieOrder",
+        related_name="ordered_movies",
+    )
+
+
+class MovieOrder(models.Model):
+    user = models.ForeignKey(
+        "users.User",
+        on_delete=models.CASCADE,
+        related_name="user_movie_order"
+    )
+
+    movie = models.ForeignKey(
+        "movies.Movie",
+        on_delete=models.CASCADE,
+        related_name="movie_order"
+    )
+
+    buyed_at = models.DateTimeField(auto_now_add=True)
+    price = models.DecimalField(max_digits=8, decimal_places=2, null=False)
+
+    def __repr__(self):
+        return f"<MovieOrder [{self.id}] - {self.price}>"
--- movies/views.py
+++ movies/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import get_object_or_404
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.views import APIView, Request, Response, status
 from rest_framework_simplejwt.authentication import JWTAuthentication
 
 from .models import Movie
 from .permissions import IsEmployeeOrReadOnly
-from .serializers import MovieSerializer
+from .serializers import MovieOrderSerializer, MovieSerializer
 
 
 class MovieView(APIView):
@@ -41,4 +42,18 @@ class MovieDetailView(APIView):
         movie.delete()
 
         return Response(status=status.HTTP_204_NO_CONTENT)
-        
+    
+
+class MovieOrderView(APIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+    def post(self, req: Request, movie_id: int) -> Response:
+        movie = get_object_or_404(Movie, id=movie_id)
+        seriaizer = MovieOrderSerializer(data=req.data)
+        seriaizer.is_valid(raise_exception=True)
+
+        seriaizer.save(user=req.user, movie=movie)
+
+        return Response(seriaizer.data, status.HTTP_201_CREATED)
+
